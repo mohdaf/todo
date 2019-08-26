18cd8d94fb11504c6121d06e39a72806abee7999
Mohammed <mabdlmajid@gmail.com>
2019-08-26 23:08:44 +0400

    added basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
+import { LoginComponent } from './login/login.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosComponent } from './todos/todos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/todos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todos',
+    component: TodosComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
