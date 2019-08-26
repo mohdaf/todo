f35a0d5b57539263e0cbc3cafc768b19512bc05a
Mohammed <mabdlmajid@gmail.com>
2019-08-26 23:01:39 +0400

    components boilerplates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    TodosComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
