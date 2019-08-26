a36555c7c487d4dab60f90f5d8a57ef1b6454368
Mohammed <mabdlmajid@gmail.com>
2019-08-26 23:58:01 +0400

    added register feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { AuthService } from './auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -18,9 +21,13 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router'
+
+@Injectable()
+export class AuthService {
+
+  private _registerUrl = "https://todo-backend.modaf.xyz/api/user/register/";
+  private _loginUrl = "https://todo-backend.modaf.xyz/api/user/token/";
+
+  constructor(private http: HttpClient,
+              private _router: Router) { }
+
+  registerUser(user) {
+    return this.http.post<any>(this._registerUrl, user);
+  }
+
+  loginUser(user) {
+    return this.http.post<any>(this._loginUrl, user);
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/login']);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  loggedIn() {
+    return !!localStorage.getItem('token');  
+  }
+}
