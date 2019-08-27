a531946558d299ea97522a646c61afdad1fc4d8d
Mohammed <mabdlmajid@gmail.com>
2019-08-27 04:07:10 +0400

    added refresh token logic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { catchError, mapTo, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
 
   private _registerUrl = "https://todo-backend.modaf.xyz/api/user/register/";
   private _loginUrl = "https://todo-backend.modaf.xyz/api/user/token/";
+  private _refreshUrl = "https://todo-backend.modaf.xyz/api/user/token/refresh/";
 
   constructor(private http: HttpClient,
               private _router: Router) { }
@@ -19,6 +21,17 @@ export class AuthService {
     return this.http.post<any>(this._loginUrl, user);
   }
 
+  refreshToken() {
+    return this.http.post<any>(this._refreshUrl, {
+      'refresh': this.getRefreshToken()
+      }).pipe(tap((tokens: {
+          access: string;
+          refresh: string;
+        }) => {
+          localStorage.setItem('token', tokens.access);
+        }));
+  }
+
   logoutUser() {
     localStorage.removeItem('token');
     this._router.navigate(['/login']);
@@ -26,6 +39,10 @@ export class AuthService {
 
   getToken() {
     return localStorage.getItem('token');
+  }
+
+  getRefreshToken() {
+    return localStorage.getItem('refresh');
   }
 
   loggedIn() {
--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -1,18 +1,59 @@
-import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { catchError, filter, take, switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+
 @Injectable()
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor(private injector: Injector){}
-  intercept(req, next) {
-    let authService = this.injector.get(AuthService);
-    let tokenizedReq = req.clone(
-      {
-        headers: req.headers.set('Authorization', 'Bearer ' + authService.getToken())
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+  constructor(public authService: AuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (this.authService.getToken()) {
+      request = this.addToken(request, this.authService.getToken());
+    }
+
+    return next.handle(request).pipe(catchError(error => {
+      if (error instanceof HttpErrorResponse && error.status === 401) {
+        return this.handle401Error(request, next);
+      } else {
+        return throwError(error);
       }
-    )
-    return next.handle(tokenizedReq)
+    }));
   }
 
+  private addToken(request: HttpRequest<any>, token: string) {
+    return request.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+
+      return this.authService.refreshToken().pipe(
+        switchMap((token: any) => {
+          this.isRefreshing = false;
+          this.refreshTokenSubject.next(token.access);
+          return next.handle(this.addToken(request, token.access));
+        }));
+
+    } else {
+      return this.refreshTokenSubject.pipe(
+        filter(token => token != null),
+        take(1),
+        switchMap(access => {
+          return next.handle(this.addToken(request, access));
+        }));
+    }
+  }
 }
